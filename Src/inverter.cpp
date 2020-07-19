d021817f49e1436a0967e3aaf6fe2c6828023ec7
Matej Planinšek <matej.planinsek@gmail.com>
2020-07-19 14:25:51 +0200

    added inverter class

---
+++ Src/inverter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by matej on 19/07/2020.
+//
+
+#include "inverter.h"
---
+++ Src/inverter.h
@@ -0,0 +1,14 @@
+//
+// Created by matej on 19/07/2020.
+//
+
+#ifndef INVERTER_4CH_INVERTER_H
+#define INVERTER_4CH_INVERTER_H
+
+
+class inverter {
+
+};
+
+
+#endif //INVERTER_4CH_INVERTER_H
